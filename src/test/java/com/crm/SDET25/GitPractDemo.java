e955c30e9171f8443199d1587992855ab9aca7f0
GiriGoudru <GiriGoudru@GiriGoudru-PC>
2021-12-09 16:40:18 +0530

    second

---
+++ src/test/java/com/crm/SDET25/GitPractDemo.java
@@ -0,0 +1,20 @@
+package com.crm.SDET25;
+
+	
+
+
+	import org.testng.annotations.Test;
+
+	public class GitPractDemo {
+	  @Test
+		public static void main(String[] args) {
+			System.out.println("hello world");
+			System.out.println("hello worldsss");
+			
+
+		}
+
+	}
+
+
+
