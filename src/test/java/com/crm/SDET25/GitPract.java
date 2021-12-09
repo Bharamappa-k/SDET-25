9af50633732343cca25585e13624d8623d0dcfba
GiriGoudru <GiriGoudru@GiriGoudru-PC>
2021-12-09 16:26:27 +0530

    first commit

---
+++ src/test/java/com/crm/SDET25/GitPract.java
@@ -0,0 +1,13 @@
+package com.crm.SDET25;
+
+import org.testng.annotations.Test;
+
+public class GitPract {
+  @Test
+	public static void main(String[] args) {
+		System.out.println("hello world");
+		
+
+	}
+
+}
