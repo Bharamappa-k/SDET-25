6bcbf532e91c5033e664a0cbb8df3d743d33fae9
GiriGoudru <GiriGoudru@GiriGoudru-PC>
2021-12-10 10:19:23 +0530

    committt12

---
+++ src/test/java/com/crm/SDET25/Demo2.java
@@ -0,0 +1,14 @@
+package com.crm.SDET25;
+
+import org.testng.annotations.Test;
+
+public class Demo2 {
+	
+	
+	@Test
+	public void demo() {
+		
+		System.out.println("hi");
+	}
+
+}
---
+++ src/test/java/com/crm/SDET25/Demo3.java
@@ -0,0 +1,13 @@
+package com.crm.SDET25;
+
+
+import org.testng.annotations.Test;
+
+public class Demo3 {
+	
+	@Test
+	public void demo() {
+		System.out.println("hekkkk");
+	}
+
+}
